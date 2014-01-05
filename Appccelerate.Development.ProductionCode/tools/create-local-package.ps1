3627e48856cc2bb2aff61d1a68007293fa43205a
Urs Enzler <urs.enzler@gmail.com>
2014-01-05 21:40:17 +0100

    added script to version, build and create nuget package locally

---
+++ Appccelerate.Development.ProductionCode/tools/create-local-package.ps1
@@ -0,0 +1,42 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$solution,
+	
+   [Parameter(Mandatory=$True, Position=2)]
+   [string]$nugetFolder
+)
+#if ($args.Count -ne 1)
+#{
+#    Write-Host "usage create-package <project_file>";
+#    Exit;
+#}
+
+#$solution = "c:\projects\appccelerate\repos\fundamentals\source\appccelerate.fundamentals.sln"  #$args[0];
+$project = [io.path]::GetFileNameWithoutExtension($solution);
+$folder = [io.directory]::GetParent($solution);
+$nuspec = "" + $folder + "\" + $project + ".nuspec";
+$version = "0.0.0";
+
+Write-Host "building nuget package for "
+Write-Host "solution = " $solution;
+Write-Host "base folder = " $folder;
+Write-Host "nuspec = " $nuspec;
+
+$location = Get-Location
+
+cd $folder
+
+$version_output = gitflowversion;
+
+$version_json = $version_output -join " " | ConvertFrom-Json
+
+$version = $version_json.NugetVersion
+
+Write-Host "version = " $version;
+
+cd $location
+
+msbuild $solution /verbosity:minimal /p:Configuration=Release
+
+nuget pack .\eventbroker\source\appccelerate.eventbroker.nuspec -outputDirectory $nugetFolder -symbols -version $version 
