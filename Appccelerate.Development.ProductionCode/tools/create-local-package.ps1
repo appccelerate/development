7829b558b8992b26540a98b541045216f4c9f37a
Urs Enzler <urs.enzler@gmail.com>
2014-05-27 15:54:08 +0200

    added return codes to create-local-package

--- Appccelerate.Development.ProductionCode/tools/create-local-package.ps1
+++ Appccelerate.Development.ProductionCode/tools/create-local-package.ps1
@@ -62,7 +62,13 @@ msbuild $solution /verbosity:minimal /p:Configuration=Release
 if ($LASTEXITCODE -ne 0)
 {
     Write-Host Failed to build
-    Exit
+    Exit 17
 }
 
 nuget pack $nuspec -outputDirectory $nugetFolder -symbols -version $version 
+
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Host Failed to build
+    Exit 42
+}
