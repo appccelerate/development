532c70deb4d55a14103bbe9a37d37b8367a93d3e
Urs Enzler <urs.enzler@gmail.com>
2016-02-05 14:29:01 +0100

    made header stylecop compliant

--- Appccelerate.Development/content/Properties/GlobalAssemblyInfo.cs
+++ Appccelerate.Development/content/Properties/GlobalAssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿//-------------------------------------------------------------------------------
-// <copyright file="GlobalAssemblyInfo.cs" company="Appccelerate">
-//   Copyright (c) 2008-2015
+﻿// <copyright file="GlobalAssemblyInfo.cs" company="Appccelerate">
+//   Copyright (c)  2008-2016
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
+//
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
 //
@@ -14,12 +14,11 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 // </copyright>
-//-------------------------------------------------------------------------------
 
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Appccelerate")]
@@ -28,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
